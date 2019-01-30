93cfb6d410fb4fe1d078f747156d40d74dbd6a10
wyf <yifan.wang@hoo.ai>
2019-01-30 14:53:09 +0800

    打包

--- src/router/components.js
+++ src/router/components.js
@@ -1,16 +1,8 @@
-
-// 定义异步组件也可这样 const Foo = resolve => require(['./Foo.vue'], resolve)
-// 定义异步组件，将组件按组分块，‘group-index’为 组名称
-
-
-let components;
-const Index = r => require.ensure([], () => r(require('../pages/index/inedx.vue')), 'group-index');
-const Apply = r => require.ensure([], () => r(require('../pages/index/apply.vue')), 'group-apply');
-
-
-components = {
-    Index,
-    Apply,
+const Index = () =>
+  import(/* webpackChunkName: "group-index" */ "@/pages/index/inedx.vue");
+const Apply = () =>
+  import(/* webpackChunkName: "group-apply" */ "@/pages/index/apply.vue");
+export default {
+  Index,
+  Apply
 };
-
-export default components;
