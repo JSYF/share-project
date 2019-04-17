e4382b1baf97aeffc0862313a4da809613aaf1fe
wujiayi <jiayi.wu@yunhan100.com>
2019-04-17 16:09:30 +0800

    host

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    // host: 'mp.xiaomingkeji.com', // can be overwritten by process.env.HOST
-    host: '127.0.0.1',
+    host: 'mp.xiaomingkeji.com', // can be overwritten by process.env.HOST
+    // host: '127.0.0.1',
     // hosr:"localhost",
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
