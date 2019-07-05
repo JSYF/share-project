e96aa61f322a17c09b0fda17449238f615608bee
wyf <yifan.wang@hoo.ai>
2019-07-05 11:05:44 +0800

    bugfix

--- src/pages/saas/index.vue
+++ src/pages/saas/index.vue
@@ -158,29 +158,11 @@
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @keyframes toBig
-    form
-      font-size: 48px;
-    to
-      font-size: 60px;
   .bg-wrap
     width: 100%;
     height: auto;
-    // background-color: #38388b;
     img
       width 100%;
-    .bg-1
-      height: 660px;
-    .bg-3
-      height: 1065px;
-    .bg-4
-      height: 1140px;
-    .bg-5
-      height: 854px;
-		.bg-6
-      height: 1020px;
-    .bg-7
-      height: 963px;
   .input-wrap
     background-color: #fff;
     padding: 50px 35px 70px;
@@ -213,67 +195,4 @@
       line-height: 95px;
       background-color: #0079ea;
       border-radius: 5px;
-  .share-box
-    position: fixed;
-    right: 12px;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-color: #f94575;
-    box-shadow: 0px 0px 10px 0px rgba(31, 32, 31, 0.2);
-    .iconfont
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translateX(-50%) translateY(-50%);
-      font-size: 48px;
-      color: #fff;
-      animation: toBig 1s linear infinite;
-    &:hover
-      opacity: 0.8;
-  .like-box
-    text-align: center;
-    height: 110px;
-    padding-top: 20px;
-    box-sizing: border-box;
-    .title
-      margin: 0 auto;
-      width: 591px;
-      height: 38px;
-      background-color: rgba(249, 69, 117, 0.1);
-      border-radius: 19px;
-      font-size: 24px;
-      color: #f94575;
-      line-height: 36px;
-      text-align: center;
-    .scroll-wrap
-      position: relative;
-      margin-top: 12px;
-      overflow: hidden;
-      height: 32px;
-    .like-show
-      display: flex;
-      justify-content: center;
-      font-size: 24px;
-      padding-bottom: 12px;
-      &.action
-        transform: translateY(-100%);
-        transition: 1s all;
-      .iconfont
-        align-self: center;
-        flex: 0 0 auto;
-        font-size: 18px;
-        color: #f94575;
-        width: 33px;
-        height: 22px;
-        background-color: rgba(249, 69, 117, 0.1);
-        margin-right: 10px;
-        position: relative;
-        &:before
-          position: absolute;
-          left: 50%;
-          top: 50%;
-          transform: translateX(-50%) translateY(-50%);
 </style>
